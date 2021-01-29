74bc0a715e7ed4648635ceef3c09261d70237274
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-29 20:57:54 -0300

    REST testado

---
+++ src/main/java/com/flavio/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.flavio.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public String listar() {
+		return "REST está funcionando!";
+	}
+	
+}
