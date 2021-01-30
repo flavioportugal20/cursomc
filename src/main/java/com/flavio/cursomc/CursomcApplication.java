60bef48be68b2eab0d6983f771ac282c29e39a4b
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-30 10:26:30 -0300

    Criando operacao de instanciacao

--- src/main/java/com/flavio/cursomc/CursomcApplication.java
+++ src/main/java/com/flavio/cursomc/CursomcApplication.java
@@ -1,14 +1,34 @@
 package com.flavio.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.flavio.cursomc.domain.Categoria;
+import com.flavio.cursomc.repositories.CategoriaRepository;
+import com.flavio.cursomc.resources.CategoriaResource;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
