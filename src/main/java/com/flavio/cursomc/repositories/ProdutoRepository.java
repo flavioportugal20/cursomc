a6b4d01e42e32b78c613fbc63018a71cbef0758f
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-30 10:53:37 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/flavio/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.flavio.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.flavio.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
